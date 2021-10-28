c4e987841e8e76878af1bbd3626a7447ef0079cc
Kaustubh Karthik <kaustubhkarthik@gmail.com>
2021-10-28 11:41:28 +0100

    Added html and svg conversion to view spacy objects

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import spacy
 from spacy import displacy
+from IPython.core.display import display, HTML
 
 nlp = spacy.load("en_core_web_sm")
 doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
-displacy.render(doc, style='dep')
+svg = displacy.render(doc, style='dep', minify='true')
+
+with open('spacy-NLP/sentence_connections.svg', 'w', encoding='utf-8') as connections:
+    connections.write(svg)
 
 for token in doc:
     print(token.text, token.pos_, token.dep_)
-    
--- spacy-tutorial.py
+++ spacy-tutorial.py
@@ -3,9 +3,12 @@ from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
 
-with open("data/wiki_us.txt", 'r') as file:
+with open(r'C:\Users\Kaustubh Karthik\Downloads\freecodecamp_spacy-main\freecodecamp_spacy-main\data\wiki_us.txt', 'r') as file:
     text = file.read()
 
 doc = nlp(text)
 
+for ent in doc.ents:
+    print(ent.text, ent.label_)
 
+displacy.serve(doc, style='ent', minify=True)
